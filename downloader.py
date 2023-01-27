7a62f5c934766517f82b9a28c6187c01fe308cd6
gir87 <123775653+gir87@users.noreply.github.com>
2023-01-27 22:09:10 +0100

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -10,7 +10,4 @@ os.rename(video,"video.mp4")
 audio = youtube.streams.filter(subtype='mp4', only_audio=True).order_by('bitrate').desc().first().download()
 os.rename(audio,"audio.mp4")
 
-video_stream = ffmpeg.input('video.mp4')
-audio_stream = ffmpeg.input('audio.mp4')
-
 os.system("ffmpeg -i audio.mp4 -i video.mp4 -async 1 -c copy merged.mp4")
